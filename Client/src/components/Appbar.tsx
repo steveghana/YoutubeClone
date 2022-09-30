d7739545f56d3269db06b9b85e6e9905a9941b14
unknown <boatengstephen707@gmail.com>
2022-09-30 02:23:15 -1100

    initial logic

--- Client/src/components/Appbar.tsx
+++ Client/src/components/Appbar.tsx
@@ -1,6 +1,14 @@
 import React from "react";
-import { FaYoutube, FaSearch } from "react-icons/fa";
-import { makeStyles } from "@material-ui/core";
+import {
+  FaYoutube,
+  FaSearch,
+  FaCamera,
+  FaBell,
+  FaUser,
+  FaMicrophone,
+} from "react-icons/fa";
+import { SearchOutlined } from "@material-ui/icons";
+import { makeStyles, Typography } from "@material-ui/core";
 const appbarStyles = makeStyles((theme) => ({
   appbar: {
     display: "flex",
@@ -19,18 +27,38 @@ const appbarStyles = makeStyles((theme) => ({
 const Appbar = () => {
   const classes = appbarStyles();
   return (
-    <nav className={classes.appbar}>
-      <div className={classes.logo}>
-        <FaYoutube />
-        YOUTUBE
+    <nav className="appbar_container">
+      <div className="appbar_logo">
+        <FaYoutube color="red" size={30} />
+        <Typography>YOUTUBE</Typography>
       </div>
-      <div className={classes.search}>
-        <input type="text" placeholder="Search" />
-        <FaSearch />
+      <div className="appbar_wrapper">
+        <div className="appbar_searchContainer">
+          <input
+            type="text"
+            className="appbar_searchbar"
+            placeholder="Search"
+          />
+          <div style={{ padding: ".2rem 1rem", color: "white" }}>
+            <SearchOutlined />
+          </div>
+        </div>
+        <div className="appbar_microphone">
+          <FaMicrophone />
+        </div>
       </div>
-      <div className={classes.camera}></div>
-      <div className={classes.notification}></div>
-      <div className={classes.profile}></div>
+
+      <div className="appbar_icons">
+        <div className={classes.camera}>
+          <FaCamera />
+        </div>
+        <div className={classes.notification}>
+          <FaBell />
+        </div>
+        <div className={classes.profile}>
+          <FaUser />
+        </div>
+      </div>
     </nav>
   );
 };
