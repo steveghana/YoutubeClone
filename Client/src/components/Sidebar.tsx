7fe7526e160689101be6c3d302e0dee6f1bd6df9
unknown <boatengstephen707@gmail.com>
2022-09-30 02:22:22 -1100

    added initial logic for sidebar components

--- Client/src/components/Sidebar.tsx
+++ Client/src/components/Sidebar.tsx
@@ -1,14 +1,28 @@
 import { Grid, makeStyles, Typography } from "@material-ui/core";
 import React from "react";
-import { FaHome, FaCompass, FaHamburger } from "react-icons/fa";
+import {
+  Menu,
+  Subscriptions,
+  LibraryMusic,
+  MusicVideo,
+  HistoryRounded,
+} from "@material-ui/icons";
+import { FaHome, FaCompass } from "react-icons/fa";
 const sidebarData = [
   { text: "Home", icon: <FaHome size={20} /> },
   { text: "Explore", icon: <FaCompass size={20} /> },
+  { text: "Shorts", icon: <MusicVideo /> },
+  { text: "Subscriptions", icon: <Subscriptions /> },
+  { text: "Library", icon: <LibraryMusic /> },
+  { text: "History", icon: <HistoryRounded /> },
 ];
 
 const Sidebar = () => {
   return (
     <Grid className="sidebar_container">
+      <div className="sidebar_hameburger-Toggle">
+        <Menu />
+      </div>
       {sidebarData.map(({ icon, text }, itemidx) => (
         <div key={itemidx}>
           <div>{icon}</div>
