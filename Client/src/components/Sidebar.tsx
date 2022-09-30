1d76492807c85fdff1874c1d772236d64060430a
unknown <boatengstephen707@gmail.com>
2022-09-30 08:19:36 -1100

    sidebar update

--- Client/src/components/Sidebar.tsx
+++ Client/src/components/Sidebar.tsx
@@ -18,13 +18,37 @@ const sidebarData = [
 ];
 
 const Sidebar = () => {
+  // const [toggleSidebar, setTogglesidebar] = React.useState(false);
+  let toggleSidebar = false;
+  const toggleRef = React.useRef(null);
+  const sidebarItemRef = React.useRef(null);
+  const expandSideBar = () => {
+    toggleSidebar = !toggleSidebar;
+    //@ts-ignore
+    let toggleSidebarClass = toggleRef?.current?.classList;
+    //@ts-ignore
+    let toggleSidebarItemClass = sidebarItemRef?.current?.querySelector(
+      ".sidebar_container_item"
+    );
+    if (toggleSidebar) {
+      toggleSidebarClass.add("toggle");
+      toggleSidebarItemClass.add("itemRow");
+    } else {
+      toggleSidebarClass.remove("toggle");
+      toggleSidebarItemClass.remove("itemRow");
+    }
+  };
   return (
-    <Grid className="sidebar_container">
-      <div className="sidebar_hameburger-Toggle">
+    <Grid ref={toggleRef} className="sidebar_container">
+      <div onClick={expandSideBar} className="sidebar_hameburger-Toggle">
         <Menu />
       </div>
       {sidebarData.map(({ icon, text }, itemidx) => (
-        <div key={itemidx}>
+        <div
+          className="sidebar_container_item"
+          //@ts-ignore
+          key={itemidx}
+        >
           <div>{icon}</div>
           <Typography variant="caption">{text}</Typography>
         </div>
